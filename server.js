a11037dcc045702d531c112e9e0ef304c428ad4a
ahmadhamzh <ahmadhamzh@ymail.com>
2021-09-07 18:58:51 +0300

    lab 08

--- server.js
+++ server.js
@@ -1,57 +1,49 @@
 'use strict'
-
+const axios = require('axios');
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
 app.use(cors())
-const weather = require('./data/weather.json')
-// && element.lon == lon && element.lat == lat
-// a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-    res.send(weather) // our endpoint function response
-})
+const weather = require('./data/weather.json');
+require('dotenv').config();
+const WEATHER_API_KEY = process.env.WEATHER_BIT_API_KEY;
+app.get('/',
+    function (req, res) { // callback function of what we should do with our request
+        res.send(weather) // our endpoint function response
+    })
 
-app.get('/weather', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
+app.get('/weather',
+    async (request, response) => {
 
-    const cityName = req.query.city_name
-    // const lon = req.query.lon
-    // const lat = req.query.lat
-    // const lonValue = re
-    if (cityName){
-        const cityNameFiltered = weather.find(element => {
-            return (element.city_name === cityName)
-        })
-        if (cityNameFiltered){
-            // let dataArr = [cityNameFiltered[0].data[0].datetime,cityNameFiltered[0].data[0].weather.description]
-            let cityNameFilteredData = cityNameFiltered.data
-            let dataArr = []
-            class Forecast {
-                constructor(date, description) {
-                  this.date = date;
-                  this.description = description;
+        const lat = request.query.lat;
+        const lon = request.query.lon;
+
+        const weatherBitUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
+        try {
+
+            const weatherBitResponse = await axios.get(`${weatherBitUrl}?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
+            if (lat && lon) {
+                let cityNameFilteredData = weatherBitResponse.data.data
+                let dataArr = []
+                class Forecast {
+                    constructor(date, description) {
+                        this.date = date;
+                        this.description = description;
+                    }
                 }
-              }
-              cityNameFilteredData.map(element => {
-                 let newOpject =  new Forecast (element.datetime, element.weather.description)
-                 dataArr.push(newOpject)
-              })
-
-            res.json(dataArr) 
-        } else {
-            res.send(weather)
+                cityNameFilteredData.map(element => {
+                    let newOpject = new Forecast(element.datetime, element.weather.description)
+                    dataArr.push(newOpject)
+                })
+                response.json(dataArr);
+    
+            } else {
+                response.send('no data found')
+            }
+        } catch (error) {
+            response.json(error.data);
         }
-            
-            
-        } else {
-            
-            res.json(weather) 
-    }
+    })
 
-     
 
-})
-
- 
 app.listen(3001) // kick start the express server to work
