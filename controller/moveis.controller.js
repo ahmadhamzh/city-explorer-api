01fd3798e23b7f5b62b175bc2edf7ece41295a83
ahmadhamzh <ahmadhamzh@ymail.com>
2021-09-10 16:28:08 +0300

    lab-10

--- controller/moveis.controller.js
+++ controller/moveis.controller.js
@@ -1,33 +1,59 @@
 'use strict'
 const axios = require('axios');
+const Cash = require('../helper/cash.helper');
 require('dotenv').config();
 const Moveisdata = require('../models/moviesdata.model');
 const MOVIES_API_KEY = process.env.MOVIE_API_KEY;
-
+let cashObject = new Cash();
 
 const movies = async (request, response) => {
-    const searchQuery = request.query.query
-    const moviesUrl = 'https://api.themoviedb.org/3/search/movie'   
 
-    try {
-        const moviesResponse = await axios.get(`${moviesUrl}?api_key=${MOVIES_API_KEY}&query=${searchQuery}`)
-        let responseData = moviesResponse.data.results
-
-        if (responseData) {
-            let movieResultArr = [];
-            responseData.map(element => {
-                let newObject = new Moveisdata (element.title, element.overview, element.vote_average, element.vote_count, element.poster_path, element.popularity, element.release_date)
-                movieResultArr.push(newObject)
-            })            
-            response.json(movieResultArr)
-        } else {
-            response.send('no data found')
-        }
-
-    } catch (error) {
-        response.send(error.data)
+    const dayInMilSec = 50000;
+    const oneDayPassed = (Date.now() - cashObject.timestamp) > dayInMilSec;
+    if (oneDayPassed) {
+      
+      cashObject = new Cash();
     }
 
+    const query = request.query.query
+    
+    const foundData = cashObject.moviesList.find(movie => {
+        return movie.query == query ;
+    });
+    if (foundData) {
+        response.json(foundData.data)
+        console.log('data from cash movies ')
+        
+    } else {
+        
+        const moviesUrl = 'https://api.themoviedb.org/3/search/movie'   
+        
+        try {
+            const moviesResponse = await axios.get(`${moviesUrl}?api_key=${MOVIES_API_KEY}&query=${query}`)
+            let responseData = moviesResponse.data.results
+            
+            if (responseData) {
+                let data = [];
+                responseData.map(element => {
+                    let newObject = new Moveisdata (element.title, element.overview, element.vote_average, element.vote_count, element.poster_path, element.popularity, element.release_date)
+                    data.push(newObject)
+                })            
+                cashObject.moviesList.push({
+                    'query' : query,
+                    'data' : data
+                })
+                response.json(data)
+                console.log(foundData)
+            } else {
+                response.send('no data found')
+            }
+            
+        } catch (error) {
+           
+            response.send(error)
+        }
+    }
+        
 }
 
 module.exports = movies;
--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -3,29 +3,55 @@ const axios = require('axios');
 require('dotenv').config();
 const Forecast = require('../models/forcast.model');
 const WEATHER_API_KEY = process.env.WEATHER_BIT_API_KEY;
-
+const Cash = require('../helper/cash.helper');
+let cashObject = new Cash();
 const weather =async (request, response) => {
+
+    const dayInMilSec = 50000;
+  const oneDayPassed = (Date.now() - cashObject.timestamp) > dayInMilSec;
+  if (oneDayPassed) {
+    
+    cashObject = new Cash();
+  }
 
     const lat = request.query.lat;
     const lon = request.query.lon;
-
-    const weatherBitUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
-    try {
-
-        const weatherBitResponse = await axios.get(`${weatherBitUrl}?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
-        if (lat && lon) {
-            let cityNameFilteredData = weatherBitResponse.data.data
-            let dataArr = []            
-            cityNameFilteredData.map(element => {
-                let newOpject = new Forecast(element.datetime, element.weather.description)
-                dataArr.push(newOpject)
-            })
-            response.json(dataArr);
-        } else {
-            response.send('no data found')
+    const foundData = cashObject.weatherForcast.find(location => {
+       return location.lat === lat && location.lon === lon
+    })
+    if (foundData) {
+        response.send(foundData.data)
+        console.log('data from cash weather');
+        
+    } else {
+        
+        const weatherBitUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
+        try {
+            
+            const weatherBitResponse = await axios.get(`${weatherBitUrl}?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
+            if (lat && lon) {
+                let cityNameFilteredData = weatherBitResponse.data.data
+                let data = []            
+                cityNameFilteredData.map(element => {
+                    let newOpject = new Forecast(element.datetime, element.weather.description)
+                    data.push(newOpject)
+                })
+                cashObject.weatherForcast.push({
+                    'lat' : lat,
+                    'lon' : lon,
+                    'data' : data
+                })
+                response.json(data);
+                // console.log(cashObject.weatherForcast)
+                console.log('data from api weather');
+                console.log(foundData);
+                
+            } else {
+                response.send('no data found')
+            }
+        } catch (error) {
+            response.json(error.data);
         }
-    } catch (error) {
-        response.json(error.data);
     }
 }
 module.exports = weather;
